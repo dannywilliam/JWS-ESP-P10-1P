9c5c3fa3bc0bee346a46c9e91c202b2f53bc2b14
dannywilliam <95993575+dannywilliam@users.noreply.github.com>
2024-01-24 17:15:30 +0700

    Mengganti Jadwal Sholat dengan hitung mundur jadwal sholat

--- JWS-ESP-P10_1P/JWS-ESP-P10_1P.ino
+++ JWS-ESP-P10_1P/JWS-ESP-P10_1P.ino
@@ -1,49 +1,3 @@
-/*
- * JADWAL WAKTU SHOLAT MENGGUNAKAN NODEMCU ESP8266, LED P10, RTC DS3231, BUZZER
- * FITUR :  JADWAL SHOLAT 5 WAKTU DAN TANBIH IMSAK, JAM BESAR, TANGGAL, SUHU, ALARAM ADZAN DAN TANBIH IMSAK,
- *          DAN HITUNG MUNDUR IQOMAH DAN UBAH WAKTU LEWAT WIFI DENGAN BROWSER.
- * 
-
-Pin on  DMD P10     GPIO      NODEMCU               Pin on  DS3231      NODEMCU                   Pin on  Buzzer       NODEMCU
-        2  A        GPIO16    D0                            SCL         D1 (GPIO 5)                       +            RX (GPIO 3)
-        4  B        GPIO12    D6                            SDA         D2 (GPIO 4)                       -            GND
-        8  CLK      GPIO14    D5                            VCC         3V
-        10 SCK      GPIO0     D3                            GND         GND
-        12 R        GPIO13    D7
-        1  NOE      GPIO15    D8
-        3  GND      GND       GND
-
-Catatan : 
-o Perlu Power Eksternal 5V ke LED P10.
-o Saat Flashing (upload program) cabut sementara pin untuk buzzer.
-
-Project Git
-- https://github.com/busel7/Arduino
-- https://github.com/busel7/Arduino/tree/master/MembuatJWS
-
-Youtube Channel : 
-- https://www.youtube.com/channel/UCEDJLuOw8cld2rvy8Sic2xA  <<-- Mohon bantu Subscribe untuk mendukung saya membuat konten Video bermanfaat lainnya :)
-
-
-Eksternal Library
-- DMDESP : https://github.com/busel7/DMDESP
-- PrayerTime : https://github.com/asmaklad/Arduino-Prayer-Times
-- RTC DS3231 : https://github.com/Makuna/Rtc
-- ArduinoJson V6 : https://github.com/bblanchon/ArduinoJson
-- F1kM_Hisab : https://github.com/wardi1971/F1kM_Hisab
-
-Diskusi Grup Untuk1000Masjid : https://www.facebook.com/groups/761058907424496/
-
-Tools : 
-- Desain bitmap dan font : http://dotmatrixtool.com
-- LittleFS Uploader : https://github.com/earlephilhower/arduino-esp8266littlefs-plugin/releases
-
-
-www.grobak.net - www.elektronmart.com
-
-Updated : 15 Mei 2020
-*/
-
 #include <FS.h>
 #include <LittleFS.h>
 #include <ArduinoJson.h>
@@ -251,18 +205,9 @@ void tampilinfo() {
       break;
 
     case 1 :
-      TampilJadwalSholat();
+      TampilCountDown();
       break;
     
   }
   
 }
-
-/*
-1. Logo
-2. Jam WIB
-3. Jam Istiwa'
-4. Tanggal
-5. Jadwal Sholat
-6. Nama Masjid
-*/
