f69b666074273b2e04fcb0ea4311e2e8a9c8f9d3
dannywilliam <95993575+dannywilliam@users.noreply.github.com>
2024-01-24 17:27:22 +0700

    Mengganti Tampilan Jadwal Sholat dengan Hitung Mundur Jadwal Sholat. Memindah Fungsi Jadwal Sholat dan Update Waktu dari FSConfig.h.

--- JWS-ESP-P10_1P/JWS.h
+++ JWS-ESP-P10_1P/JWS.h
@@ -1,3 +1,7 @@
+int jwsNow, jwsNext;
+int cHor, cMin, cSec;
+int xHor, xMin, xSec;
+
 //----------------------------------------------------------------------
 // FORMAT TEKS
 
@@ -331,8 +335,8 @@ void TampilSaatSholat() {
   static uint32_t det;
 
   Disp.setFont(ElektronMart5x6);
-  textCenter(0, "LURUSKAN DAN");
-  textCenter(8, "RAPATKAN SHAF");
+  textCenter(0, "LURUSKAN");
+  textCenter(8, "RAPATKAN");
 
   if (cM - pM >= 1000) {
     pM = cM;
@@ -348,8 +352,150 @@ void TampilSaatSholat() {
   
 }
 
+//----------------------------------------------------------------------
+void CekSholat() {  
 
+  int hours, minutes, seconds;
 
+  // Cek jadwal Imsak
+  get_float_time_parts(times[0], hours, minutes);
+  minutes = minutes + configjws.ihti;
+
+  if (minutes < 10) {
+    
+    minutes = 60 - minutes;
+    hours --;
+    
+  } else {
+    
+    minutes = minutes - 10 ;
+    
+  }
+
+  // Cek apakah sudah imsak atau belum
+  if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+    jwsNow = 6;
+    jwsNext = 7;
+    xHor = hours;
+    xMin = minutes;
+  } else {
+    // Cek jadwal Subuh
+    get_float_time_parts(times[0], hours, minutes);
+    minutes = minutes + configjws.ihti;
+
+    if (minutes >= 60) {
+      minutes = minutes - 60;
+      hours ++;
+    }
+
+    if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+      jwsNow = 7;
+      jwsNext = 0;
+      xHor = hours;
+      xMin = minutes;
+    } else {
+      // Cek jadwal Terbit
+      get_float_time_parts(times[1], hours, minutes);
+      minutes = minutes - configjws.ihti;
+
+      if (minutes >= 60) {
+        minutes = minutes - 60;
+        hours ++;
+      }
+
+      if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+        jwsNow = 0;
+        jwsNext = 1;
+        xHor = hours;
+        xMin = minutes;
+      } else {
+        // Cek jadwal Dzuhur
+        get_float_time_parts(times[2], hours, minutes);
+        minutes = minutes + 4;
+
+        if (minutes >= 60) {
+          minutes = minutes - 60;
+          hours ++;
+        }
+
+        if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+          jwsNow = 1;
+          jwsNext = 2;
+          xHor = hours;
+          xMin = minutes;
+        } else {
+          // Cek jadwal Ashar
+          get_float_time_parts(times[3], hours, minutes);
+          minutes = minutes + configjws.ihti;
+
+          if (minutes >= 60) {
+            minutes = minutes - 60;
+            hours ++;
+          }
+
+          if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+            jwsNow = 2;
+            jwsNext = 3;
+            xHor = hours;
+            xMin = minutes;
+          } else {
+            // Cek jadwal Maghrib
+            get_float_time_parts(times[5], hours, minutes);
+            minutes = minutes + configjws.ihti;
+
+            if (minutes >= 60) {
+              minutes = minutes - 60;
+              hours ++;
+            }
+
+            if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+              jwsNow = 3;
+              jwsNext = 5;
+              xHor = hours;
+              xMin = minutes;
+            } else {
+              // Cek jadwal Isya'
+              get_float_time_parts(times[6], hours, minutes);
+              minutes = minutes + configjws.ihti;
+
+              if (minutes >= 60) {
+                minutes = minutes - 60;
+                hours ++;
+              }
+
+              if ((rJam < hours) or (rJam == hours && rMen <= minutes)) {
+                jwsNow = 5;
+                jwsNext = 6;
+                xHor = hours;
+                xMin = minutes;
+              } else {
+                // Cek jadwal Imsak
+                get_float_time_parts(times[0], hours, minutes);
+                minutes = minutes + configjws.ihti;
+
+                if (minutes < 10) {
+                  
+                  minutes = 60 - minutes;
+                  hours --;
+                  
+                } else {
+                  
+                  minutes = minutes - 10 ;
+                  
+                }
+
+                jwsNow = 6;
+                jwsNext = 7;
+                xHor = hours;
+                xMin = minutes;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
 //----------------------------------------------------------------------
 //TAMPILAN RUNNING TEKS
@@ -460,79 +606,82 @@ void TampilTanggal() {
   
 }
 
-//--------------------------------------
-// TAMPILKAN JADWAL SHOLAT
+//----------------------------------------------------------------------
+// TAMPILKAN COUNTDOWN
 
-void TampilJadwalSholat() {
+void TampilCountDown() {
 
-  static uint8_t i;
+  char TimeName[][8] = {"SUBUH","TERBIT","DZUHUR","ASHAR","TRBNM","MAGHRIB","ISYA'","IMSAK"};
+  char TampilMundur[9];
+  String sholat = TimeName[jwsNext];
+
+  static uint8_t y;
+  static uint8_t d;           
   static uint32_t pM;
   uint32_t cM = millis();
-  
-  char sholat[7];
-  char jam[5];
-  char TimeName[][8] = {"SUBUH","TERBIT","DZUHUR","ASHAR","TRBNM","MAGHRIB","ISYA'"};
-  int hours, minutes;  
 
-  if (cM - pM >= 3000) {
-    
-    pM = cM;
-    Disp.drawRect(0,0,31,15,0,0);
+  static uint32_t pMPulse;
+  static uint8_t pulse;
 
-    //if (i == 1) {i = 2;} // Abaikan Terbit
-    if (i == 4) {i = 5;} // Abaikan Terbenam
-
-    get_float_time_parts(times[i], hours, minutes);
-  
-    if (i==2) {
-      minutes = minutes + 4;
-    } else {
-      minutes = minutes + configjws.ihti;
-    }
-    
-    if (minutes >= 60) {
-      minutes = minutes - 60;
-      hours ++;
-    }
-
-    String sholat = TimeName[i];
-    sprintf(jam,"%02d:%02d", hours, minutes);     
-    
-    Disp.setFont(ElektronMart5x6);
-    textCenter(0, sholat);
-    textCenter(8, jam);
-
-    i++;
-
-    if (i > 7) {
-      get_float_time_parts(times[0], hours, minutes);
-      minutes = minutes + configjws.ihti;
-      if (minutes < 11) {
-        minutes = 60 - minutes;
-        hours --;
-      } else {
-        minutes = minutes - 10 ;
-      }
-      sprintf(jam,"%02d:%02d", hours, minutes);
-      Disp.drawRect(0,0,31,15,0,0);
-      Disp.setFont(ElektronMart5x6);
-      textCenter(0, "IMSAK");
-      textCenter(8, jam);
-      
-      if (i > 8) {
-        i = 0;
-        Disp.drawRect(0,0,31,15,0,0);
-        tmpinfo = 0;
-        tmpjws = 3;
-      }
-      
-    }      
-    
+  if (cM - pMPulse >= 500) {
+    pMPulse = cM;
+    pulse++;
   }
   
+  if (pulse > 8) {
+    pulse=0;
+  }
+
+  if(cM - pM > 35) { 
+    if(d == 0 and y < 20) {
+      pM=cM;
+      y++;
+    }
+    if(d  == 1 and y > 0) {
+      pM=cM;
+      y--;
+    }    
+  }
+  
+  if(cM - pM > 5000 and y == 20) { // Jam akan tampil selama 5 detik
+    d=1;
+    Disp.drawRect(0,0,31,15,0,0);
+  }
+  
+  if(y == 0 and d == 1) {
+    d=0;
+    Disp.drawRect(0,0,31,15,0,0);
+    tmpinfo = 0;
+    tmpjws = 3;
+  }
+  
+  cHor = xHor - rJam;
+  cMin = xMin - rMen - 1;
+  cSec = 60 - rDet;
+
+  if (cHor < 0) {
+    cHor = cHor + 24;
+  }
+
+  if (cMin < 0) {
+    cMin = cMin + 60;
+    cHor --;
+  }
+
+  if (cHor > 0) {
+    sprintf(TampilMundur, "-%02d:%02d", cHor, cMin);
+  } else if (cMin > 0) {
+    sprintf(TampilMundur, "-%02d:%02d", cMin, cSec);
+  } else {
+    sprintf(TampilMundur, "-%02d", cSec);
+  }
+
+  //JTampilkan Hitungan Mundur
+  Disp.setFont(ElektronMart5x6);
+  textCenter(0, sholat);
+  textCenter(8, TampilMundur);
+  
 }
-
-
 
 //----------------------------------------------------------------------
 // TAMPILKAN JAM
@@ -706,6 +855,88 @@ void JamBesarIst(uint16_t x) {
   } else {
     Disp.setFont(BigArab6x16);
     Disp.drawText(x+15, y-20, ":");
+  }
+  
+}
+
+// PENGHITUNG JADWAL SHOLAT
+
+void JadwalSholat() {
+
+  /*
+    CALCULATION METHOD
+    ------------------
+    Jafari,   // Ithna Ashari
+    Karachi,  // University of Islamic Sciences, Karachi
+    ISNA,     // Islamic Society of North America (ISNA)
+    MWL,      // Muslim World League (MWL)
+    Makkah,   // Umm al-Qura, Makkah
+    Egypt,    // Egyptian General Authority of Survey
+    Custom,   // Custom Setting
+
+    JURISTIC
+    --------
+    Shafii,    // Shafii (standard)
+    Hanafi,    // Hanafi
+
+    ADJUSTING METHOD
+    ----------------
+    None,        // No adjustment
+    MidNight,   // middle of night
+    OneSeventh, // 1/7th of night
+    AngleBased, // angle/60th of night
+
+    TIME IDS
+    --------
+    Fajr,
+    Sunrise,
+    Dhuhr,
+    Asr,
+    Sunset,
+    Maghrib,
+    Isha
+  
+  */
+
+  set_calc_method(Karachi);
+  set_asr_method(Shafii);
+  set_high_lats_adjust_method(AngleBased);
+  set_fajr_angle(20);
+  set_isha_angle(18);
+
+  get_prayer_times(rTah, rBul, rTgl, configjws.latitude, configjws.longitude, configjws.zonawaktu, times);
+
+}
+
+//-----------------------------------
+// UPDATE WAKTU
+
+void UpdateWaktu() {
+
+  static long pM;
+  static uint16_t d;
+  
+
+  if (millis() - pM > 1000) {
+    d++;
+    if (d < 2) {
+      BacaRTC();
+      JadwalSholat();
+      CekSholat();
+    } else if (d < 60) { // Update setiap 60 detik
+      rDet++;
+      if (rDet > 59) {
+        rMen++;
+        rDet=0;
+        if (rMen > 59) {
+          rJam++;
+          rMen = 0;
+        }
+      }
+    } else {
+      d = 0; 
+    }
+    pM = millis();    
   }
   
 }
